7d6cbcc1330fb1dd07be8c5ed82ad818e1f73862
gandikotesubramanya <2025cs_gandikotesubramanya_c@nie.ac.in>
2026-09-23 10:48:40 +0530

    Update 02main1.py

--- DAY02/DAY03/02main1.py
+++ DAY02/DAY03/02main1.py
@@ -43,3 +43,32 @@ def get_ticket_read_all():
     docs = ticket_collection.find()
     tickets = [ticket_helper(doc) for doc in docs]
     return tickets
+
+@app.get("/tickets/{id}",response_model=TicketResponse)
+def get_ticket_read_one(id:str):
+    if not ObjectId.is_valid(id):
+        raise HTTPException(status_code=403, detail="Invalid ticket ID")
+    doc = ticket_collection.find_one({"_id": ObjectId(id)})
+    if not doc:
+        raise HTTPException(status_code=404, detail="Ticket not found")
+    return ticket_helper(doc)
+
+@app.put("/tickets/{id}",response_model=TicketResponse)
+def ticket_update(id:str, payload:TicketCreate):
+    if not ObjectId.is_valid(id):
+        raise HTTPException(status_code=403, detail="Invalid ticket ID")
+    ticket_dict = payload.model_dump()
+    result = ticket_collection.update_one({"_id": ObjectId(id)}, {"$set": ticket_dict})
+    if result.matched_count == 0:
+        raise HTTPException(status_code=404, detail="Ticket not found")
+    new_ticket = ticket_collection.find_one({"_id": ObjectId(id)})
+    return ticket_helper(new_ticket)
+
+@app.delete("/tickets/{id}")
+def ticket_delete(id:str):
+    if not ObjectId.is_valid(id):
+        raise HTTPException(status_code=403, detail="Invalid ticket ID")
+    result = ticket_collection.delete_one({"_id": ObjectId(id)})
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Ticket not found")
+    return {"message": "Ticket deleted successfully"}
