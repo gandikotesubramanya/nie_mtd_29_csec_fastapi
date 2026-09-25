1e68713af6060a66df6ad412e47fb98fc9b06f63
gandikotesubramanya <2025cs_gandikotesubramanya_c@nie.ac.in>
2026-09-25 09:56:14 +0530

    day

---
+++ DAY04/frontend/calc_app/src/About.jsx
@@ -0,0 +1,22 @@
+export default function About() {
+
+return (
+    <div>
+      <h1>Calculators</h1>
+      <h2>square calc</h2> 
+      <p>square calculate will calculate the square of the given number</p>
+      <ul>
+      <li>takes a number</li>
+      <li>prints the square of number</li>
+  </ul>
+
+  <h2>sum calc</h2>
+  <ol>
+      <li>takes 2 numbers</li>
+      <li>prints the sum of numbers</li>
+  </ol>
+  <img src="images.png">x</img>
+  <img src="image2.png">x</img>
+    </div>
+  )
+}
---
+++ DAY04/frontend/calc_app/src/App.jsx
@@ -0,0 +1,14 @@
+src/App.jsx
+import About from "./About"
+import Square from "./Square"
+
+function App() {
+  return (
+    <>
+      <p>My Application</p>
+      <hr/>
+      <Square/>
+      <hr/>
+    </>
+  )
+}
